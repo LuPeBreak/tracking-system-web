5bea88b1643601cfb2a646d664ab6a2ea7d16562
LuPeBreak <lfbmrj15@gmail.com>
2024-07-01 15:47:11 -0300

    Update: Implemented shadcn-ui , tailwindcss, prettier, eslint and react-router-dom

---
+++ src/routes.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './_layouts/app'
+import { AuthLayout } from './_layouts/auth'
+import { Button } from './components/ui/button'
+import { NotFound } from './pages/404'
+import { Error } from './pages/error'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const router: any = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element:<Button>Dashboard</Button>
+        ,
+      },
+    ],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <Button>Login</Button>,
+      },
+      {
+        path: '/sign-up',
+        element: <Button>Sign up</Button>,
+      },
+    ],
+  },
+
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
